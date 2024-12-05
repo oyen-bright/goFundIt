f4124121fad7237410a6e3157e7f8048f8ac518a
oyen-bright <oyenbrihight@gmail.com>
2024-12-05 20:28:05 +0100

    Implement encryption and decryption functionality; enhance email handling and configuration management

---
+++ internal/encryption/encryption.go
@@ -0,0 +1,33 @@
+package encryptor
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+
+	"github.com/oyen-bright/goFundIt/internal/utils/validator"
+)
+
+type Data struct {
+	Data  string
+	Email string
+}
+
+func (e Data) generateEncryptionKey(secretKey string) (string, error) {
+	// Validate the secret key
+	if secretKey == "" {
+		return "", errors.New("invalid secret key")
+	}
+
+	// Validate the email
+	isValidEmail := validator.ValidateEmail(e.Email)
+	if !isValidEmail {
+		return "", errors.New("invalid email")
+	}
+
+	// Generate a SHA-256 hash of the email and secret key
+	hash := sha256.Sum256([]byte(e.Email + secretKey))
+
+	// Encode the first 24 bytes of the hash to a base64 string
+	return base64.StdEncoding.EncodeToString(hash[:24]), nil
+}
---
+++ internal/utils/validator/validator.go
@@ -0,0 +1,11 @@
+package validator
+
+import (
+	"regexp"
+)
+
+func ValidateEmail(email string) bool {
+	var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	return emailRegex.MatchString(email)
+
+}
