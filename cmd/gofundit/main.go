d115aadb6d22f343418d71911e3781ea8a3b282b
oyen-bright <oyenbrihight@gmail.com>
2024-12-08 21:34:06 +0100

    Add authentication routes and JWT integration; update configuration for API key and email settings

--- cmd/gofundit/main.go
+++ cmd/gofundit/main.go
@@ -1,48 +1,65 @@
 package main
 
 import (
-	"fmt"
-
-	appConfig "github.com/oyen-bright/goFundIt/config"
+	"github.com/gin-gonic/gin"
+	"github.com/oyen-bright/goFundIt/config"
 	"github.com/oyen-bright/goFundIt/config/providers"
+	"github.com/oyen-bright/goFundIt/internal/auth"
 	"github.com/oyen-bright/goFundIt/internal/database"
 	"github.com/oyen-bright/goFundIt/internal/otp"
+	"github.com/oyen-bright/goFundIt/internal/utils/jwt"
 	"github.com/oyen-bright/goFundIt/pkg/email"
 	"github.com/oyen-bright/goFundIt/pkg/encryption"
+	"github.com/oyen-bright/goFundIt/pkg/middlewares"
+	"gorm.io/gorm"
 )
+
+func initialize() (*config.AppConfig, *gorm.DB) {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		panic(err)
+	}
+	db, err := database.Init(*cfg)
+	if err != nil {
+		panic(err)
+	}
+	return cfg, db
+}
 
 func main() {
 
-	cfg, err := appConfig.LoadConfig()
-	if err != nil {
-		panic(err)
-
-	}
-	db, err := database.Init(*cfg)
+	// Initialize database and application configurations
+	cfg, db := initialize()
 	// migrations.DropOtpTable(db)
 	defer database.Close(db)
-	if err != nil {
-		panic(err)
-	}
 
+	// Create a new Gin router
+	r := gin.Default()
+
+	// Register general middlewares
+	r.Use(middlewares.APIKeyAuthMiddleware(cfg.XAPIKey))
+
+	// Initialize encryption and email services
 	encryptor := encryption.New(cfg.EncryptionKey)
+	emailer := email.New(providers.EmailSMTP, cfg.EmailConfig)
 
-	emailer := email.New(providers.EmailSMTP, email.EmailConfig{
-		Host:           cfg.EmailHost,
-		Port:           cfg.EmailPort,
-		From:           cfg.EmailName,
-		Username:       cfg.EmailUsername,
-		Password:       cfg.EmailPassword,
-		SendGridAPIKey: cfg.SendGridAPIKey,
-	})
+	// Initialize jwt
+	jwt := jwt.New(cfg.JWTSecret)
 
-	otpService := otp.OtpService{DB: db, Emailer: &emailer, Encryptor: encryptor}
-	err = otpService.RequestOTP("bright@krotrust.com", "Bright")
-	// isVerified, err := otpService.VerifyOTP("bright@krotrust.com", "MSCKKR")
-	// fmt.Print(isVerified)
+	// Initialize repositories
+	authRepo := auth.Repository(db)
+	otpRepo := otp.Repository(db)
 
-	fmt.Print(err)
+	// Create service instances
+	otpService := otp.Service(otpRepo, emailer, *encryptor)
+	authService := auth.Service(authRepo, *encryptor, jwt)
 
-	// router := gin.Default()
+	// Create handler instances
+	authHandler := auth.Handler(otpService, authService)
 
+	// Register routes
+	authHandler.RegisterRoutes(r.Group("/auth"), []gin.HandlerFunc{})
+
+	// Start the server on the specified port
+	r.Run(cfg.Port)
 }
