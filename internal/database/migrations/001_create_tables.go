f3cb0d5a7e8e88812c32444aae82a5f0249c83d4
oyen-bright <oyenbrihight@gmail.com>
2024-12-16 14:18:42 +0100

    Refactor OTP handling: update migrations to include new models, enhance OTP service with error logging, and implement OTP deletion functionality

--- internal/database/migrations/001_create_tables.go
+++ internal/database/migrations/001_create_tables.go
@@ -1,34 +1,38 @@
 package migrations
 
 import (
+	"github.com/oyen-bright/goFundIt/internal/activity"
 	"github.com/oyen-bright/goFundIt/internal/auth"
+	"github.com/oyen-bright/goFundIt/internal/campaign"
+	"github.com/oyen-bright/goFundIt/internal/contributor"
 	"github.com/oyen-bright/goFundIt/internal/otp"
+	"github.com/oyen-bright/goFundIt/internal/payment"
 
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) error {
 	// Applying migration
+	// DropOtpTable(db)
 	err := db.AutoMigrate(
 		&otp.Otp{},
+		&campaign.Campaign{},
+		&campaign.CampaignImage{},
+		&activity.Activity{},
+		&contributor.Contributor{},
 		&auth.User{},
+		&payment.Payment{},
 	)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
-func ClearOtpTable(db *gorm.DB) error {
-	// Deleting all data in the otp table
-	err := db.Exec("DELETE FROM otp").Error
-	if err != nil {
-		return err
-	}
-	return nil
-}
+
 func DropOtpTable(db *gorm.DB) error {
 	// Dropping the otp table
-	err := db.Migrator().DropTable(&otp.Otp{}, &auth.User{})
+	err := db.Migrator().DropTable(&campaign.Campaign{}, &campaign.CampaignImage{}, auth.User{}, contributor.Contributor{}, otp.Otp{}, activity.Activity{})
 	if err != nil {
 		return err
 	}
