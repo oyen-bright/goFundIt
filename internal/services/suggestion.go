62e2f8badad8fa76e448f655fd8cbc91e26df74a
oyen-bright <oyenbrihight@gmail.com>
2024-12-20 23:31:48 +0100

    Implement activity suggestion features; add ActivitySuggestion model, create SuggestionService interface, and implement suggestionService with methods for generating activity suggestions.

---
+++ internal/services/suggestion.go
@@ -0,0 +1,53 @@
+package services
+
+import (
+	ai "github.com/oyen-bright/goFundIt/internal/ai/interfaces"
+	"github.com/oyen-bright/goFundIt/internal/models"
+	services "github.com/oyen-bright/goFundIt/internal/services/interfaces"
+	"github.com/oyen-bright/goFundIt/pkg/errs"
+	"github.com/oyen-bright/goFundIt/pkg/logger"
+)
+
+type suggestionService struct {
+	aIService       ai.AIService
+	campaignService services.CampaignService
+	logger          logger.Logger
+}
+
+func NewSuggestionService(aIService ai.AIService, campaignService services.CampaignService, logger logger.Logger) services.SuggestionService {
+	return &suggestionService{
+		aIService:       aIService,
+		logger:          logger,
+		campaignService: campaignService,
+	}
+}
+
+// GenerateActivitySuggestions generates activity suggestions based on the campaign description.
+func (s *suggestionService) GetActivitySuggestions(campaignID string) ([]models.ActivitySuggestion, error) {
+	//Validate campaign
+
+	campaign, err := s.campaignService.GetCampaignByID(campaignID)
+	if err != nil {
+		return nil, err
+	}
+
+	//Get suggestions from AI service
+	suggestions, err := s.aIService.GenerateActivitySuggestions(campaign.Title + " " + campaign.Description)
+
+	if err != nil {
+		return nil, errs.InternalServerError(err).Log(s.logger)
+	}
+
+	return suggestions, nil
+
+}
+
+func (s *suggestionService) GetActivitySuggestionsViaText(content string) ([]models.ActivitySuggestion, error) {
+	suggestions, err := s.aIService.GenerateActivitySuggestions(content)
+
+	if err != nil {
+		return nil, errs.InternalServerError(err).Log(s.logger)
+	}
+
+	return suggestions, nil
+}
