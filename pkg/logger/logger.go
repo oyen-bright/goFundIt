877a628780e5c29d6a49e10978d87802222ada00
oyen-bright <oyenbrihight@gmail.com>
2024-12-26 02:20:38 +0100

    Refactor logger to support structured logging with Sentry integration for info and error messages

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,19 +1,73 @@
 package logger
 
+// import (
+// 	"log"
+// )
+
+// type Logger interface {
+// 	Log(message string)
+// }
+
+// type SimpleLogger struct{}
+
+// func New() Logger {
+// 	return &SimpleLogger{}
+// }
+
+// func (l *SimpleLogger) Log(message string) {
+// 	log.Println(message)
+// }
+
 import (
 	"log"
+
+	"github.com/getsentry/sentry-go"
 )
 
 type Logger interface {
-	Log(message string)
+	Info(message string, fields map[string]interface{})
+	Error(err error, message string, fields map[string]interface{})
 }
 
-type SimpleLogger struct{}
+type logger struct {
+	enabled bool
+}
 
 func New() Logger {
-	return &SimpleLogger{}
+	return &logger{
+		enabled: true,
+	}
 }
 
-func (l *SimpleLogger) Log(message string) {
+func (l *logger) Info(message string, fields map[string]interface{}) {
 	log.Println(message)
+
+	if !l.enabled {
+		return
+	}
+
+	sentry.WithScope(func(scope *sentry.Scope) {
+		scope.SetLevel(sentry.LevelInfo)
+		if fields != nil {
+			scope.SetExtras(fields)
+		}
+		sentry.CaptureMessage(message)
+	})
+}
+
+func (l *logger) Error(err error, message string, fields map[string]interface{}) {
+	log.Println(err.Error())
+
+	if !l.enabled {
+		return
+	}
+
+	sentry.WithScope(func(scope *sentry.Scope) {
+		scope.SetLevel(sentry.LevelError)
+		scope.SetExtra("info", message)
+		if fields != nil {
+			scope.SetExtras(fields)
+		}
+		sentry.CaptureException(err)
+	})
 }
